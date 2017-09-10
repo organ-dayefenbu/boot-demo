db30842b3ae046729fc34f6e90d91c9bef6d78b3
li.shuming <550124552@qq.com>
2017-09-10 16:15:38 +0800

    版本兼容变更

--- src/main/java/com/demo/boot/warinit/ServletInitializer.java
+++ src/main/java/com/demo/boot/warinit/ServletInitializer.java
@@ -1,7 +1,7 @@
 package com.demo.boot.warinit;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 import com.demo.boot.main.Application;
 
